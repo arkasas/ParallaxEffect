9deaf7f2d0b26248d60ccc811992334b60cf4bc6
Arkadiusz Pituła <arkadiusz.pitula@gymnas.io>
2017-04-30 18:04:14 +0200

    add parallax effect

--- ParallaxEffect/ParallaxEffect/ViewController.swift
+++ ParallaxEffect/ParallaxEffect/ViewController.swift
@@ -10,9 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var background: UIImageView!
+    @IBOutlet weak var famale: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        applyMotionEffect(toView: background, magintude: 10);
+        applyMotionEffect(toView: famale, magintude: -20);
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,5 +25,20 @@ class ViewController: UIViewController {
     }
 
 
+}
+extension ViewController{
+    func applyMotionEffect(toView view: UIView, magintude: Float){
+        let xMotion = UIInterpolatingMotionEffect(keyPath: "center.x", type: .tiltAlongHorizontalAxis);
+        xMotion.minimumRelativeValue = -magintude;
+        xMotion.maximumRelativeValue = magintude;
+        
+        let yMotion = UIInterpolatingMotionEffect(keyPath: "center.y", type: .tiltAlongVerticalAxis);
+        yMotion.maximumRelativeValue = magintude;
+        yMotion.minimumRelativeValue = -magintude;
+        
+        let group = UIMotionEffectGroup();
+        group.motionEffects = [xMotion, yMotion];
+        view.addMotionEffect(group);
+    }
 }
 
